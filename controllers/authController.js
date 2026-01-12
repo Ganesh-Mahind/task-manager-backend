5dd85ae6ba1ed529c3578a10bdeafddb1f545028
Ganesh MentorChamp <ganesh.internchamp23@gmail.com>
2026-01-12 16:45:57 +0530

    Initial production backend release

---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async(req,res)=>{
+  const {name,email,password} = req.body;
+  const hash = await bcrypt.hash(password,10);
+  const user = await User.create({name,email,password:hash});
+  res.json(user);
+}
+
+exports.login = async(req,res)=>{
+  const {email,password} = req.body;
+  const user = await User.findOne({email});
+  if(!user || !await bcrypt.compare(password,user.password))
+    return res.status(401).json({message:"Invalid"});
+  const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+  res.json({token});
+}
